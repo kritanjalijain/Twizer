47381cacc764b71705c3bdaba94bde4ef0271100
Kritanjali Jain <kritanjalijain@gmail.com>
2021-10-31 20:33:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
 from flask import Flask, request, jsonify
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_marshmallow import Marshmallow
 import os
 import requests
 from requests.structures import CaseInsensitiveDict
 import json
+import tweepy as tw
 from os import environ
 from dotenv import load_dotenv
 
+
 app = Flask(__name__)
 load_dotenv()
+token = os.getenv('BEARER_TOKEN')
+
+consumerKey =  os.getenv('CONSUMER_KEY')
+consumerSecret =  os.getenv('CONSUMER_SECRET')
+accessToken =  os.getenv('ACCESS_TOKEN')
+accessTokenSecret =  os.getenv('ACCESS_SECRET')
+
+authenticate = tw.OAuthHandler(consumerKey, consumerSecret) 
+        
+# Set the access token and access token secret
+authenticate.set_access_token(accessToken, accessTokenSecret) 
+    
+# Creating the API object while passing in auth information
+api = tw.API(authenticate, wait_on_rate_limit = True)
+
+
 
 @app.route('/api/sentiment', methods=['POST'])
 def sentiment():
@@ -17,25 +33,17 @@ def sentiment():
     user = request.json['user']
     num = request.json['num']
 
-    token = os.getenv('BEARER_TOKEN')
+    # token = ''
 
     # text = request.json['text']
     headers = CaseInsensitiveDict()
     # headers["Accept"] = "application/json"
     headers["Authorization"] = "Bearer "+token
-    print('https://api.twitter.com/2/users/by/username/'+user)
+
     resp = requests.get('https://api.twitter.com/2/users/by/username/'+user, headers=headers).json()
     print(resp)
     # resp2 = requests.get('https://api.twitter.com/2/users/'+resp['data']['id']+'/tweets', headers=headers).json() 
-    resp2 = requests.get('https://api.twitter.com/2/users/'+resp['data']['id']+'/tweets?tweet.fields=public_metrics&'+'max_results='+num, headers=headers).json() 
-    #resp3 = requests.get('https://api.twitter.com/2/users/'+resp['data']['id']+'/tweets?tweet.fields=organic_metrics&'+'max_results='+num, headers=headers).json() 
-    #print(resp3)
-    
-    url = "https://api.twitter.com/2/users/by/username/"+ user +"?user.fields=created_at,description,entities,id,location,name,pinned_tweet_id,profile_image_url,protected,url,username,verified,withheld&expansions=pinned_tweet_id&tweet.fields=attachments,author_id,conversation_id,created_at,entities,geo,id,in_reply_to_user_id,lang,possibly_sensitive,referenced_tweets,source,text,withheld"
-    payload={}
-    headers = {}
-    response = requests.request("GET", url, headers=headers, data=payload)
-    print(response.text)
+    resp2 = requests.get('https://api.twitter.com/2/users/'+resp['data']['id']+'/tweets?tweet.fields=public_metrics,entities,lang,geo,created_at&'+'max_results='+num, headers=headers).json() 
 
     from tensorflow.keras.models import load_model
     from tensorflow.keras.preprocessing.text import Tokenizer
@@ -53,29 +61,26 @@ def sentiment():
     with open('./lstm/preprocess.pickle', 'rb') as handle:
         tokenizer = pickle.load(handle)
 
-    # tokenizer.fit_on_texts(text)
-    # tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
 
     def predict_class(text):
         '''Function to predict sentiment class of the passed text'''
         
         sentiment_classes = ['Negative', 'Neutral', 'Positive']
         max_len=50
-        # print(text)
+        print(text)
         # Transforms text to a sequence of integers using a tokenizer object
-        # tokenizer = Tokenizer()
-        
+         
         xt = tokenizer.texts_to_sequences(text)
         print(xt)
         # Pad sequences to the same length
         xt = pad_sequences(xt, padding='post', maxlen=max_len)
-        print(xt)
+        #print(xt)
         # Do the prediction using the loaded model
         yt = model.predict(xt).argmax(axis=1)
         print(model.predict(xt))
         # Print the predicted sentiment
         k= sentiment_classes[yt[0]]
-        # print(k)
+        print(k)
         return k
 
     final_dict = {}
@@ -85,59 +90,127 @@ def sentiment():
         tweet = i['text']
         analysis = predict_class([tweet])
         i['sentiment']=analysis
-        # final_dict[tweet]=analysis
-        # temp = final_dict
-        # final_dict = {}
-        # res_array.append(temp)
-        # temp = {}
-    print(resp2)
 
-    for i in resp2['data']:
-        tweet = i['text']
-    print(resp2)
+    return jsonify({ "result": resp2['data'] })
 
-    # return jsonify({ "result": predict_class([text])})
-    # return jsonify({ "result": res_array })
-    return jsonify({ "result":  resp2['data'] })
 
-# @app.route('/api/lstm', methods=['POST'])
-# def lstm():
-#     text = request.json['text']
-#     from tensorflow.keras.models import load_model
-#     from tensorflow.keras.preprocessing.text import Tokenizer
-#     from tensorflow.keras.preprocessing.sequence import pad_sequences
-#     model = load_model('./lstm/best_model.h5')
-#     import pickle
-#     max_words = 5000
-#     max_len=50
-#     tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
-#     with open('./lstm/preprocess.pickle', 'rb') as handle:
-#         tokenizer = pickle.load(handle)
-#     # tokenizer.fit_on_texts(text)
-#     # tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
-#     def predict_class(text):
-#         '''Function to predict sentiment class of the passed text'''
+
+
+
+
+
+
+
+
+@app.route('/api/user', methods=['POST'])
+def user():
+
+    user = request.json['user']
+    # num = request.json['num']
+
+    # token = ''
+
+    # text = request.json['text']
+    headers = CaseInsensitiveDict()
+    # headers["Accept"] = "application/json"
+    headers["Authorization"] = "Bearer "+token
+
+    resp = requests.get('https://api.twitter.com/2/users/by/username/'+user, headers=headers).json()
+
+    print(resp)
+
+
+    user_followers = api.get_user(user_id=resp['data']['id'])
+
+    final_res = {
+        'user':         user,
+        'Followers':    user_followers.followers_count,
+        'Friends':      user_followers.friends_count,
+        'Tweets':       user_followers.statuses_count,
+        'Tweets liked': user_followers.favourites_count
+    }
+
+    return jsonify({ "Result": final_res })
+    # return jsonify({ "result": len(resp2['data']) })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@app.route('/api/hashtag', methods = ['POST'])
+def tags():
+    tag = request.json['hashtag']
+    num = request.json['num']
+    tag = '#' + tag
+
+
+
+    from tensorflow.keras.models import load_model
+    from tensorflow.keras.preprocessing.text import Tokenizer
+    from tensorflow.keras.preprocessing.sequence import pad_sequences
+    
+    model = load_model('./lstm/best_model.h5')
+    
+    import pickle
+
+    max_words = 5000
+    max_len=50
+
+    tokenizer = Tokenizer(num_words=max_words, lower=True, split=' ')
+
+    with open('./lstm/preprocess.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    def predict_class(text):
+        '''Function to predict sentiment class of the passed text'''
         
-#         sentiment_classes = ['Negative', 'Neutral', 'Positive']
-#         max_len=50
-#         # print(text)
-#         # Transforms text to a sequence of integers using a tokenizer object
-#         # tokenizer = Tokenizer()
-        
-#         xt = tokenizer.texts_to_sequences(text)
-#         print(xt)
-#         # Pad sequences to the same length
-#         xt = pad_sequences(xt, padding='post', maxlen=max_len)
-#         print(xt)
-#         # Do the prediction using the loaded model
-#         yt = model.predict(xt).argmax(axis=1)
-#         print(model.predict(xt))
-#         # Print the predicted sentiment
-#         k= sentiment_classes[yt[0]]
-#         # print(k)
-#         return k
+        sentiment_classes = ['Negative', 'Neutral', 'Positive']
+        max_len=50
+        print(text)
+        # Transforms text to a sequence of integers using a tokenizer object
+       
+        xt = tokenizer.texts_to_sequences(text)
+        #print(xt)
+        # Pad sequences to the same length
+        xt = pad_sequences(xt, padding='post', maxlen=max_len)
+        #print(xt)
+        # Do the prediction using the loaded model
+        yt = model.predict(xt).argmax(axis=1)
+        #print(model.predict(xt))
+        #print(yt)
+        # Print the predicted sentiment
+        k= sentiment_classes[yt[0]]
+        #print(k)
+        return k
+    
+    tweets = []
+    for tweet in tw.Cursor(api.search_tweets, q=tag).items(int(num)):
+        tweets.append(tweet.text)
 
-#     return jsonify({ "result": predict_class([text])})
+    analysis = {}
+    
+    final_res = []
+
+    for tweet in tweets:
+        analysis['text'] = tweet
+        #print(tweet)
+        analysis['sentiment'] = predict_class([tweet])
+        final_res.append(analysis)
+        analysis = {}
+
+
+    return jsonify({ 'result': final_res })
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
